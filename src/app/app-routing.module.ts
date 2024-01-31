2edf153625307d169455f672450333af89ab9f5c
prasathkumara <prasathkumar.a@cynnent.com>
2024-01-31 10:18:05 +0530

    screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { Screen2Component } from './screen2/screen2.component';
 import { Screen3Component } from './screen3/screen3.component';
 
 const routes: Routes = [
-  { path: '', component: Screen1Component },
+  { path: 'screen1', component: Screen1Component },
   { path: 'screen2', component: Screen2Component },
   { path: 'screen3', component: Screen3Component },
-  { path: '', redirectTo: '', pathMatch: 'full' }, // Default route
+  { path: '', redirectTo: 'screen1', pathMatch: 'full' }, // Default route
 ];
 
 @NgModule({
